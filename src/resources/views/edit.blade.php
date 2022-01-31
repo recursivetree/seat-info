adff8abacc937372694e54cb08497eaef1b7310c
recursive_tree <recursivetreemail@gmail.com>
2022-01-31 20:53:16 +0100

    editor helper

--- src/resources/views/edit.blade.php
+++ src/resources/views/edit.blade.php
@@ -50,15 +50,28 @@
                                        value="{{ $article->name }}">
                             </div>
 
-                            <div class="d-flex w-100 form-group flex-column">
+                            <div class="d-flex w-100 form-group flex-column align-items-start">
                                 <div>
                                     <label for="text">{{ trans('info::info.article_content') }}</label>
                                 </div>
+
+                                <div class="btn-group mb-1 btn-group-sm">
+                                    <button type="button" class="btn btn-secondary" id="button-insert-heading"><i class="fas fa-heading"></i></button>
+                                    <button type="button" class="btn btn-secondary" id="button-insert-paragraph"><i class="fas fa-paragraph"></i></button>
+                                    <button type="button" class="btn btn-secondary" id="button-insert-bold"><i class="fas fa-bold"></i></button>
+                                    <button type="button" class="btn btn-secondary" id="button-insert-italic"><i class="fas fa-italic"></i></button>
+                                    <button type="button" class="btn btn-secondary" id="button-insert-link"><i class="fas fa-link"></i></button>
+                                    <button type="button" class="btn btn-secondary" id="button-insert-image"><i class="far fa-image"></i></button>
+                                    <button type="button" class="btn btn-secondary" id="button-insert-list"><i class="fas fa-list"></i></button>
+                                    <button type="button" class="btn btn-secondary" id="button-insert-list-ol"><i class="fas fa-list-ol"></i></button>
+                                    <button type="button" class="btn btn-secondary" id="button-insert-color"><i class="fas fa-palette"></i></button>
+                                </div>
+
                                 <div class="d-flex w-100" style="max-height: 60vh; min-height: 20vh;">
-                                    <div class="w-50 m-1">
+                                    <div class="w-50">
                                         <textarea name="text" style="resize: none;" class="form-control monospace-font text-sm h-100" id="text" placeholder="{{ trans('info::info.article_content_placeholder') }}" required>{{ $article->text }}</textarea>
                                     </div>
-                                    <div class="w-50 m-1">
+                                    <div class="w-50 ml-1">
                                         <div class="form-control h-100 overflow-auto" id="editor-preview-target">
 
                                         </div>
@@ -152,6 +165,57 @@
 
             textarea.addEventListener("input",render_preview)
             render_preview()
+
+            function update_textarea(selectionStart,selectionEnd,replace){
+                const startPos = textarea.selectionStart
+                const endPos = textarea.selectionEnd
+
+
+                const oldText = textarea.value
+
+                const startText = selectionStart ? selectionStart : ""
+                const centerText = replace ? replace: oldText.substring(startPos,endPos)
+                const endText = selectionEnd ? selectionEnd : ""
+
+                const newText = oldText.substring(0,startPos) + startText + centerText + endText + oldText.substring(endPos)
+
+                textarea.value = newText
+
+                textarea.focus()
+                textarea.selectionStart = startPos
+                textarea.selectionEnd = startPos + startText.length + centerText.length + endText.length
+
+
+                render_preview()
+            }
+
+            document.getElementById("button-insert-heading").addEventListener("click",function (){
+                update_textarea("<h1>","</h1>",null)
+            })
+            document.getElementById("button-insert-paragraph").addEventListener("click",function (){
+                update_textarea("<p>","</p>",null)
+            })
+            document.getElementById("button-insert-bold").addEventListener("click",function (){
+                update_textarea("<b>","</b>",null)
+            })
+            document.getElementById("button-insert-italic").addEventListener("click",function (){
+                update_textarea("<i>","</i>",null)
+            })
+            document.getElementById("button-insert-link").addEventListener("click",function (){
+                update_textarea("<a href=\"seatinfo:article/\">","</a>",null)
+            })
+            document.getElementById("button-insert-image").addEventListener("click",function (){
+                update_textarea(null,null,"<img src=\"seatinfo:resource/\" alt=\"description of the image\">")
+            })
+            document.getElementById("button-insert-list").addEventListener("click",function (){
+                update_textarea(null,null,"<ul>\n    <li>\n    </li>\n    <li>\n    </li>\n    <li>\n    </li>\n</ul>")
+            })
+            document.getElementById("button-insert-list-ol").addEventListener("click",function (){
+                update_textarea(null,null,"<ol>\n    <li>\n    </li>\n    <li>\n    </li>\n    <li>\n    </li>\n</ol>")
+            })
+            document.getElementById("button-insert-color").addEventListener("click",function (){
+                update_textarea("<color color=\"red\">","</color>",null)
+            })
         });
     </script>
 @endpush
