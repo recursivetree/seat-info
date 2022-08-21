175e8bf86788960027ea44ff79aa54915cd7a3cc
recursive_tree <recursivetreemail@gmail.com>
2022-08-21 20:17:36 +0200

    resource+article claim commands

--- src/InfoServiceProvider.php
+++ src/InfoServiceProvider.php
@@ -4,12 +4,15 @@ namespace RecursiveTree\Seat\InfoPlugin;
 
 use RecursiveTree\Seat\InfoPlugin\Acl\ArticlePolicy;
 use RecursiveTree\Seat\InfoPlugin\Acl\ResourcePolicy;
+use RecursiveTree\Seat\InfoPlugin\Model\Article;
+use RecursiveTree\Seat\InfoPlugin\Model\Resource;
 use RecursiveTree\Seat\InfoPlugin\Observers\RoleObserver;
 use Seat\Services\AbstractSeatPlugin;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Gate;
 use Seat\Web\Models\Acl\Role;
-
+use Illuminate\Support\Facades\Artisan;
+use Seat\Web\Models\User;
 
 class InfoServiceProvider extends AbstractSeatPlugin
 {
@@ -38,6 +41,81 @@ class InfoServiceProvider extends AbstractSeatPlugin
         Gate::define('info.resource.edit',[ResourcePolicy::class,"edit"]);
 
         Role::observe(RoleObserver::class);
+
+
+        //artisan commands
+
+        //claim articles
+        Artisan::command('seatinfo:articles:claim {user} {--all}', function () {
+            //get user
+            $user = $this->argument('user');
+            $user = User::find($user) ?? User::where("name",$user)->first();
+
+            if($user===null){
+                $this->error("The specified user was not found!");
+                return;
+            }
+
+            if($this->option("all")){
+                //ask for confirmation again
+                if ($this->confirm('Do you really want to overwrite ALL owners? (including articles with a valid owner!)')) {
+                    $articles = Article::all();
+                } else {
+                    $this->warn("Cancelling operation");
+                    return;
+                }
+            } else {
+                $articles = Article::where("owner",null)->get();
+            }
+
+            $count = $articles->count();
+            if (!$this->confirm("Do you really want to update the owner of $count article(s)? This can't be undone!")) {
+                return;
+            }
+
+            foreach ($articles as $article){
+                $article->owner = $user->id;
+                $article->save();
+            }
+
+            $this->info("Updated the owner of $count article(s).");
+        });
+
+        //claim resources
+        Artisan::command('seatinfo:resources:claim {user} {--all}', function () {
+            //get user
+            $user = $this->argument('user');
+            $user = User::find($user) ?? User::where("name",$user)->first();
+
+            if($user===null){
+                $this->error("The specified user was not found!");
+                return;
+            }
+
+            if($this->option("all")){
+                //ask for confirmation again
+                if ($this->confirm('Do you really want to overwrite ALL owners? (including resources with a valid owner!)')) {
+                    $resources = Resource::all();
+                } else {
+                    $this->warn("Cancelling operation");
+                    return;
+                }
+            } else {
+                $resources = Resource::where("owner",null)->get();
+            }
+
+            $count = $resources->count();
+            if (!$this->confirm("Do you really want to update the owner of $count resources(s)? This can't be undone!")) {
+                return;
+            }
+
+            foreach ($resources as $article){
+                $article->owner = $user->id;
+                $article->save();
+            }
+
+            $this->info("Updated the owner of $count resources(s).");
+        });
     }
 
     public function register(){
