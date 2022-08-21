327e7afc53084b6b6802785dbb8c0d0b7ce591b8
recursive_tree <recursivetreemail@gmail.com>
2022-08-21 19:59:43 +0200

    restrain pin and public changes to editable articles

--- src/Http/Controllers/InfoController.php
+++ src/Http/Controllers/InfoController.php
@@ -51,6 +51,8 @@ class InfoController extends Controller
         $article = Article::find($request->data);
 
         if ($article !== null) {
+            Gate::authorize("info.article.edit", $article->id);
+
             $article->public = true;
             $article->save();
 
@@ -74,6 +76,8 @@ class InfoController extends Controller
         $article = Article::find($request->data);
 
         if ($article !== null) {
+
+            Gate::authorize("info.article.edit", $article->id);
 
             $article->public = false;
             $article->save();
@@ -108,6 +112,8 @@ class InfoController extends Controller
             ]);
             return redirect()->back();
         } else {
+            Gate::authorize("info.article.edit", $article->id);
+
             $article->pinned = true;
             $article->save();
 
@@ -134,6 +140,8 @@ class InfoController extends Controller
             ]);
             return redirect()->back();
         } else {
+            Gate::authorize("info.article.edit", $article->id);
+
             $article->pinned = false;
             $article->save();
 
